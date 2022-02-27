924560550e7968d1f09000982098777b8e8ea9c0
Carl <carl.alingalan@gmail.com>
2022-02-27 16:09:25 +0800

    Fix sorting by price

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,7 +2,7 @@ export const { MONGODB_URI, HASH_SALT, PORT } = process.env;
 
 export const SORT_VALUES = {
   NAME: { name: 1 },
-  PRICE_ASC: { "price.USD": 1 },
-  PRICE_DESC: { "price.USD": -1 },
+  PRICE_ASC: { priceUSD: 1 },
+  PRICE_DESC: { priceUSD: -1 },
   CLASS: { class: 1 },
 };
