8525f41f78171aa283ae57fdc730eb65500ff6e1
Carl <carl.alingalan@gmail.com>
2022-02-27 15:08:46 +0800

    Fix linting issue

--- src/server.js
+++ src/server.js
@@ -6,17 +6,19 @@ import express from "express";
 import mongoose from "mongoose";
 import schema from "@src/schema";
 
-import { MONGODB_URI } from "@common/constants";
+import { MONGODB_URI, PORT } from "@common/constants";
 import { graphqlHTTP } from "express-graphql";
 
 const app = express();
 
 mongoose.connect(MONGODB_URI, (error) => {
   if (error) {
+    // eslint-disable-next-line no-console
     console.log("MongoDB Error: ", error);
   }
 });
 
 app.use("/graphql", graphqlHTTP({ graphiql: true, schema }));
 
-app.listen(5000, () => console.log("Express is running in port 5000"));
+// eslint-disable-next-line no-console
+app.listen(PORT, () => console.log("Express is running in port 5000"));
